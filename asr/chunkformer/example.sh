75b9e70e17d510f339da1076752620bedb399d8e
tienpm <phamminhtien2405@gmail.com>
2025-06-29 07:55:12 +0700

    feature: chunkformer asr

---
+++ asr/chunkformer/example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+AUDIO_FILE="./data/common_voice_vi_23397238.wav"
+TOTAL_BATCH_DURATION=14400
+CHUNK_SIZE=64
+LEFT_CONTEXT_SIZE=128
+RIGHT_CONTEXT_SIZE=128
+
+python decode.py \
+--long_form_audio "$AUDIO_FILE" \
+--total_batch_duration "$TOTAL_BATCH_DURATION" \
+--chunk_size "$CHUNK_SIZE" \
+--left_context_size "$LEFT_CONTEXT_SIZE" \
+--right_context_size "$RIGHT_CONTEXT_SIZE"
