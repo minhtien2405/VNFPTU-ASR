0c3cd4065a05dc8c27f9e3b1688fd193966304cd
tienpm <phamminhtien2405@gmail.com>
2025-07-01 14:53:11 +0700

    fix: ubuntu server setting

--- asr/phowhisper/scripts/train.sh
+++ asr/phowhisper/scripts/train.sh
@@ -4,4 +4,4 @@ REGION=${1:-All}
 CONFIG_PATH="asr/phowhisper/configs/config.yaml"
 
 echo "Starting training for region: $REGION"
-python asr/phowhisper/cli.py train --config $CONFIG_PATH --region $REGION
+python3 asr/phowhisper/cli.py train --config $CONFIG_PATH --region $REGION
