c7833b6ab7f38ab7ada3fefda99e14ff6ab1e997
tienpm <phamminhtien2405@gmail.com>
2025-07-02 18:39:44 +0700

    feat: trainer with multi gpu device

--- asr/phowhisper/scripts/train.sh
+++ asr/phowhisper/scripts/train.sh
@@ -4,4 +4,4 @@ REGION=${1:-All}
 CONFIG_PATH="asr/phowhisper/configs/config.yaml"
 
 echo "Starting training for region: $REGION"
-python3 asr/phowhisper/cli.py train --config $CONFIG_PATH --region $REGION
+CUDA_LAUNCH_BLOCKING=1 python3 asr/phowhisper/cli.py train --config $CONFIG_PATH --region $REGION
